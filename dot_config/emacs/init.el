e7b346be184a25ed80f85736a4951273e4a8a2ba
ansxor <darienreese@gmail.com>
2024-12-31 09:21:18 -0500

    can we fix the custom file? maybe!?

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -2,7 +2,7 @@
 (load secrets-file :no-error-if-file-is-missing)
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
-(add-hook 'elpaca-after-init-hook #'(lambda () (load custom-file :no-error-if-file-is-missing)))
+(load custom-file :no-error-if-file-is-missing))
 
 (defvar elpaca-installer-version 0.8)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
